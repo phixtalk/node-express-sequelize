61b2fc635ef97fa55592e96dfeeee0a1b9ab7512
Ofoefule Christian <ofoefule.c@gmail.com>
2021-07-08 20:23:47 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require("express");
+const { get_landing, submit_lead } = require("./controllers/landing");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", get_landing);
+router.post("/", submit_lead);
+
+module.exports = router;
