8ed46ca125c4328493f982c5f72ccaef37224a17
Ofoefule Christian <ofoefule.c@gmail.com>
2021-07-10 00:43:50 +0100

    implemented edit and delete functions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require("express");
-const { get_landing, submit_lead } = require("../controllers/landing");
+const {
+  get_landing,
+  submit_lead,
+  show_leads,
+  show_lead,
+  show_edit_lead,
+  edit_lead,
+  delete_lead,
+} = require("../controllers/landing");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", get_landing);
 router.post("/", submit_lead);
+router.get("/leads", show_leads);
+router.get("/lead/:lead_id", show_lead);
+router.get("/lead/:lead_id/edit", show_edit_lead);
+router.post("/lead/:lead_id/edit", edit_lead);
+router.post("/lead/:lead_id/delete", delete_lead);
 
 module.exports = router;
